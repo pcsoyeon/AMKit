2fb9fc0e69939ba6c1c64d82b52050748be14501
kim so yeon <sykim@autocrpt.io>
2023-08-21 10:53:55 +0900

    [Chore] init 코드 주석처리

--- Sources/AMKit/Map/AMKit_MapVC.swift
+++ Sources/AMKit/Map/AMKit_MapVC.swift
@@ -25,15 +25,15 @@ public class AMKit_MapVC: UIViewController {
     
     // MARK: - Initializer
     
-    init(lat: Double, lng: Double) {
-        self.lat = lat
-        self.lng = lng
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    public init(lat: Double, lng: Double) {
+//        self.lat = lat
+//        self.lng = lng
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
